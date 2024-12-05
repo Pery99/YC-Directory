6ad736612a936c2ca463de0f085db990fa933eef
pery <oluwapery99@gmail.com>
2024-12-04 21:02:46 -0800

    sanity setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,7 +25,7 @@ export default async function Home({
       <section className="pink_container">
         <h1 className="heading">
           Pitch your Startup, <br />
-          Connect with enterpreneurs
+          Connect with entrepreneurs
         </h1>
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
